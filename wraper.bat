a3c68cc9221441395dc9f8ff43403b46f1fc096e
ZMSkif <31103343+ZMSkif@users.noreply.github.com>
2022-05-08 03:55:20 +0300

    WRAP
    WRAP

---
+++ wraper.bat
@@ -0,0 +1,2 @@
+python wrapper.py user,post,Poștă,pošta,posta,Poczta,poster,Paštas,correspondência,correo,posta,e-posta,clave,parola,wachtwoord,Heslo,hasło,senha,mail,pass,pw,salt,solt,hash,md5,sha1,hex
+pause
---
+++ wrapper_config.py
@@ -0,0 +1,95 @@
+#coding: utf-8
+
+# proxy types: http|https|socks4|socks5
+
+
+DEBUG = True # False - without logs
+#DEBUG = False # False - without logs
+
+URLS_FILE = 'site.txt'
+SQLi_SAVE_FILE = 'goodsqli.txt'
+BRUTE_SAVE_FILE = 'goodbrute.txt'
+
+LOG_FILE = 'wrapper_session.log'
+
+# Режим дампов
+DUMP = True
+STRUCT = False
+DUMP_ALL = False
+Check_SQLi = False
+
+# Кол-во строк для дампа
+start = 1
+stop = 100000000
+
+# Поиск файла robots
+Robots = True
+ROBOTS_SAVE = 'goodrobots.txt'
+
+# Проверка ссылок на популярные домен
+Check_List = True
+
+# Брут CMS - не используется
+BRUTE_CMS = False
+THREADS_BRUTE = 100
+# Пробив эксплоитами
+EXPLOITS_CMS = True
+
+# Поиск админки и бэкапов
+ADMIN_FIND = False
+FILES_FIND = False
+FILES_SAVE = 'filesave.txt'
+FILES_PAGE = 'files.txt'
+
+# Файлы для сохранения найденных страниц
+ADMIN_PAGE = 'adminpage.txt'
+ADMIN_SAVE = 'adminsave.txt'
+PASSW_FILE = 'passw.txt'
+
+# Папка для сохранения найденных админок
+ADMIN_FOLDER = 'admdump'
+
+# Взлом админки с помощью SQLi
+ADMIN_HACK_SAVE = 'adminhacksave.txt'
+
+# Колонки для дампов
+COLUMN_DUMP = 'user,Poștă,pošta,posta,Poczta,poster,Paštas,correspondência,correo,posta,e-posta,clave,parola,wachtwoord,Heslo,hasło,senha,mail,pass,pw,salt,solt,hash,md5,sha1,hex'
+
+# Паук
+SPIDER = True
+SPIDER_LINKS = '10'
+
+# Сохранение дампов
+SQLMAP_DUMPS = 'dumps'
+WRAPPER_TXT_DUMPS = 'txt_dumps'
+DUMP_FORMAT='CSV'
+
+# Время подключения и время ответа СУБД
+time_sec = 30
+TIMEOUT = 240 # sec
+
+# Сохранять сайты с алексой выше
+ALEXA_CHECK = False
+ALEXA = 1000000
+
+# Тамперы по умолчанию
+TAMPER = 'space2comment,space2plus,randomcase'
+
+PROXY = False # False if work without it
+DUMP_COLUMN_LIMIT = 20
+PROXY_TYPE = 'socks5'
+PROXY_URL = '' # first
+PROXY_FILE = '' # second if not first
+PROXY_USERNAME = ''
+PROXY_PASSWORD = ''
+URLS_LIMIT = 10000000
+DUMP_FOLDER = 'dumps_folder'
+DUMP_COLUMN_LIMIT = 20 # 91 - 100 ...
+THREADS = 35
+
+# Удаление пустых папок
+DELETE=False
+
+RISK = 3
+LEVEL = 5
+RETRIES = '5'
